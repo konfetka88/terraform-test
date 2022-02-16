e3cf069d5fa023211cde87c412e1b8fa8fe148e4
konfetka88 <gaitova.aida93@gmail.com>
2022-02-16 06:36:06 +0000

    addint first user and group

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -1,3 +1,18 @@
 resource "aws_iam_user" "lb" {
-  name = "loadbalancer"
+  name = "userCreatedWithTerraform"
+}
+
+resource "aws_iam_user" "multiuser" {
+  name     = each.key
+  for_each = toset(["bob", "sam", "lisa"])
+}
+
+resource "aws_iam_group" "myTest" {
+  name = "myTest"
+}
+
+resource "aws_iam_group_membership" "developers_team" {
+  name  = "developers-group-membership"
+  users = [aws_iam_user.lb.name]    # or "userCreatedWithTerraform"
+  group = aws_iam_group.myTest.name # or "myTest"
 }
